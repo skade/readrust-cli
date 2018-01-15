1f9c1c7d05369f829f55b45492897e2079069080
Florian Gilcher <florian.gilcher@asquera.de>
2018-01-15 14:25:18 +0100

    Load and deserialise feed

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+pub static URL: &'static str = "http://readrust.net/rust2018/feed.json";
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Feed {
+    pub version: String,
+    pub title: String,
+    pub home_page_url: String,
+    pub feed_url: String,
+    pub description: String,
+    pub author: Author,
+    pub items: Vec<Item>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Author {
+    name: String,
+    url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Item {
+    id: String,
+    title: String,
+    context_text: String,
+    url: String,
+    date_published: String,
+    author: Author,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate readrust;
+extern crate reqwest;
+extern crate serde_json;
+
 fn main() {
-    println!("Hello, world!");
+    let mut resp = reqwest::get(readrust::URL).unwrap();
+
+    assert!(resp.status().is_success());
+
+    let data = resp.text().unwrap();
+
+    let feed = serde_json::from_str::<readrust::Feed>(&data).unwrap();
+
+    println!("feed = {:?}", feed);
 }
