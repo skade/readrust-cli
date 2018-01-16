1e0675861ebf5eaf4bc74d99d7a77be72bb546cb
Florian Gilcher <florian.gilcher@asquera.de>
2018-01-16 23:58:00 +0100

    Add two command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 extern crate readrust;
+extern crate clap;
 extern crate reqwest;
 extern crate serde_json;
 #[macro_use]
 extern crate prettytable;
 
-fn main() {
-    let client = reqwest::Client::builder()
-        .proxy(reqwest::Proxy::http("http://localhost:3128").unwrap())
-        .build().unwrap();
+use clap::{Arg, App, SubCommand};
+use readrust::{Feed, URL, Item};
 
-    let mut resp = client.get(readrust::URL).send().unwrap();
+fn get_feed() -> Feed {
+    let client = reqwest::Client::new();
+
+    let mut resp = client.get(URL).send().unwrap();
 
     assert!(resp.status().is_success());
 
     let data = resp.text().unwrap();
 
-    let feed = serde_json::from_str::<readrust::Feed>(&data).unwrap();
+    serde_json::from_str::<readrust::Feed>(&data).unwrap()
+}
+
+fn print_count(feed: &Feed) {
+    println!("Number of posts: {}", feed.items.len());
+}
+
+fn print_feed_table<'a, I: Iterator<Item=&'a Item>>(items: I) {
     let mut table = prettytable::Table::new();
 
-    // Add a row per time
     table.add_row(row!["Title", "Author", "Link"]);
 
-    // Print the table to stdout
-    for item in feed.items {
+    for item in items {
         let title = if item.title.len() >= 50 {
                         &item.title[0..50]
                     } else {
@@ -33,4 +40,30 @@ fn main() {
     }
 
     table.printstd();
+}
+
+fn main() {
+    let matches = App::new("readrust")
+                          .version("0.1")
+                          .author("Florian G. <florian.gilcher@asquera.de>")
+                          .about("Reads readrust.net")
+                          .args_from_usage(
+                              "-n, --number=[NUMBER] 'Number of posts'
+                              -c, --count            'Just counts the number'")
+                          .get_matches();
+
+    let feed = get_feed();
+
+    if matches.is_present("count") {
+        print_count(&feed);
+    } else {
+        let iter = feed.items.iter();
+
+        if let Some(string) = matches.value_of("number") {
+            let number = string.parse().unwrap();
+            print_feed_table(iter.take(number))
+        } else {
+            print_feed_table(iter)
+        }
+    }
 }
